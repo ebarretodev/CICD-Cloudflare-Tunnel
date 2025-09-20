4e99641cd7de8e078ba9ef602393d302e0394c76
Eliabel Barreto <eliabeljunior@gmail.com>
2025-09-20 00:33:10 -0300

    teste

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!\n Teste Primeiro deploy pela pipeline do Github no dns do cloudflare')
+    res.send('Eu te amo THAINARA, minha linda.')
 })
 
 app.listen(port, () => {
